b37819a147bd7fdb63d1e60f9b4ae97e0c5f8c4b
Jacqueline930 <jacqueline.lopez.930@gmail.com>
2018-07-25 09:25:04 -0500

    fixed toyproblem

--- argumentElements/argumentElements.js
+++ argumentElements/argumentElements.js
@@ -2,13 +2,5 @@
 // itself.
 // augmentElements([[],[3],[7]], 5); // [[5],[3,5],[7,5]]
 var augmentElements = function(array, aug) {
-  result = result || [];
-  if(i>arr.length-1) {
-    return result;
-  } else {
-    result.push(arr[i].concat(num));
-    augment(arr,i+1,num,result);
-  }
-  return result;
-};
+  
 };
