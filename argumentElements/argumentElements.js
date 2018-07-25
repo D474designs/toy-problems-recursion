cbcc39024b24b81988defef6a48bc2c7419d4758
Jacqueline930 <jacqueline.lopez.930@gmail.com>
2018-07-25 17:05:08 -0500

    letterTally

--- argumentElements/argumentElements.js
+++ argumentElements/argumentElements.js
@@ -1,6 +1,17 @@
 // 33. Augument every element in a list with a new value where each element is an array
 // itself.
-// augmentElements([[],[3],[7]], 5); // [[5],[3,5],[7,5]]
+// augmentElements([[],[3],[7]], 5);
+// [[5],[3,5],[7,5]]
 var augmentElements = function(array, aug) {
-  
+  var newArray = []
+
+  if(array.length === 0){
+    return []
+  }
+
+  newArray.push(array.splice(0, 1)[0])
+  newArray[0].push(aug)
+
+  return  newArray.concat(augmentElements(array, aug))
+
 };
