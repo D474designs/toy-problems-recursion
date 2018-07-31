2ac5b199406fdae653af5ad7cf7670f900831f9f
D474th3D357R0Y3R <jcazares117@gmail.com>
2018-07-31 09:14:08 -0500

    add swapingPairs solution

--- swapinPairs/swapingPairs.js
+++ swapinPairs/swapingPairs.js
@@ -1,22 +1,35 @@
 /*Swap all elements in pairs. If the length of the list is odd, the last element stays
 in place. [2,7,8,3,1,4] Returns [7,2,3,8,4,1] [3,6,8,1,5] Returns [6,3,1,8,5]*/
 
-var swapElements = function(array) {
-  //START SOLUTUION//
-  var i = 0;
-  var j = 1;
-  function swappo(array) {
-    if ((array.length % 2 === 1) && (i === array.length - 1)) {
-      var shift = array.splice(i, 1);
-      array.splice(j, 0, shift);
-      i += 2;
-      j += 2;
-      return swappo(array);
-    } else {
-    return array;
-      }
-  //END SOLUTUION//
-  }
-  return swappo(array);
+// var swapElements = function(array) {
+//   //START SOLUTUION//
+//   var i = 0;
+//   var j = 1;
+//   function swappo(array) {
+//     if ((array.length % 2 === 1) && (i === array.length - 1)) {
+//       var shift = array.splice(i, 1);
+//       array.splice(j, 0, shift);
+//       i += 2;
+//       j += 2;
+//       return swappo(array);
+//     } else {
+//     return array;
+//       }
+//   //END SOLUTUION//
+//   }
+//   return swappo(array);
+// };
+// swapElements([3,6,8,1,5]);
+
+var swapElements = function(array, i=0, result=[]) {
+  // result = result || [];
+  if (i > array.length) {
+    return result;
+  } else {
+      result.push(array[i + 1]);
+      result.push(array[i]);
+      return swapElements(array, i + 2, result);
+    }
+return result;
 };
 swapElements([3,6,8,1,5]);
